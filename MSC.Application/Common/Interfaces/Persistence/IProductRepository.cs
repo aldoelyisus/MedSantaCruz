9dc3624b59843e1ee055a8e33c05bcecde970734
Aldo Escobedo <aldojesva@gmail.com>
2023-06-06 01:47:24 -0600

    hay errores

---
+++ MSC.Application/Common/Interfaces/Persistence/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MSC.Application;
+using MSC.Application.csproj;
+
+namespace MSC.Application
+{
+    internal interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(uint id);
+        Task Add(Product product);
+        Task Edit(Product product);
+        Task Delete(uint id);
+    }
+}
